92c6ab97ce6553523dc12f2a1e36cb44c80524a2
Jam Hernandez <jamarck96@gmail.com>
2019-03-01 17:36:33 -0500

    rmeoved some extra things form my previous implementation

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -2,7 +2,6 @@ from PIL import Image
 import numpy as np
 from JsonProcessing import JsonProcessing
 from random import randint
-from AudioLogic import AudioLogic
 import time
 from JsonProcessing import JsonProcessing
 
@@ -16,7 +15,6 @@ class ImageProcessing(object):
         accert(jp.isinitialized() == True)
         self.genosToShow = genosToShow
         self.colors = colors
-        self.al = AudioLogic()
 
     def displayImage(self):
         width = 700
@@ -35,7 +33,6 @@ class ImageProcessing(object):
         img.show()
         print("Genotype {} save...".format("all"))
         print("Done.")
-        # Display the image
 
     def getImage(self, height, width, channels, geno=None):
         position = 0
@@ -54,22 +51,6 @@ class ImageProcessing(object):
         geno = self.genoList[position]
         if(targetGeno):
             if (geno in targetGeno):
-                self.al.play()
                 return self.colors[geno]["r"], self.colors[geno]["g"], self.colors[geno]["b"]
-            time.sleep(.001)
             return 0, 0 ,0
         return self.colors[geno]["r"], self.colors[geno]["g"], self.colors[geno]["b"]
-
-    def playWithColors(self, colors):
-        toReturn = dict()
-        r, g, b = 135, 48, 100
-
-        for geno in colors:
-            toReturn[geno] = dict()
-            toReturn[geno]["r"] = r
-            toReturn[geno]["g"] = g
-            toReturn[geno]["b"] = b
-            r += 10
-            g += 10
-            b += 10
-        return toReturn
