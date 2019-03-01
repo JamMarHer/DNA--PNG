66c6f789272f5f51b456c80621917243dd09b734
Jam Hernandez <jamarck96@gmail.com>
2019-03-01 18:11:02 -0500

    solved a couple problems

--- main.py
+++ main.py
@@ -3,21 +3,27 @@ from JsonProcessing import JsonProcessing
 from ImageProcessing import ImageProcessing
 
 COLORS_PATH ='colorValues.json'
-REPORT_PATH = None # TODO Add your path 
+REPORT_PATH = None
 
 
 
 if __name__ == '__main__':
     jsonP = None
+
+    fromCommand = False
     if REPORT_PATH is None:
         if (sys.argv[1:][0].split(".")[-1] != "txt"):
             # TODO eh it could technically in other format, good for now.
             print("Please make sure that the file is in txt format")
         else:
+            fromCommand = True
             jsonP = JsonProcessing(sys.argv[1:][0])
     else:
         jsonP = JsonProcessing(REPORT_PATH)
     jsonP.initialize()
     colorsDic = jsonP.getJSONasDic(COLORS_PATH)
-    imageP = ImageProcessing(jsonP, colorsDic, sys.argv[1:])
+    if fromCommand:
+        imageP = ImageProcessing(jsonP, colorsDic, sys.argv[2:])
+    else:
+        imageP = ImageProcessing(jsonP, colorsDic, sys.argv[1:])
     imageP.displayImage()
