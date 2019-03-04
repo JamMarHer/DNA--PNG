715a20f78b27feab1fe58aa1036aec03fb7636a8
Jam Hernandez <jamarck96@gmail.com>
2019-03-04 13:30:53 -0500

    typo

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     fromCommand = False
     if REPORT_PATH is None:
         if (sys.argv[1:][0].split(".")[-1] != "txt"):
-            # TODO eh it could technically in other format, good for now.
+            # TODO eh it could technically be in other format, good for now.
             print("Please make sure that the file is in txt format")
         else:
             fromCommand = True
